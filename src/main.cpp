b157dbd66e104e67dbd16fea5392f60da53ef3af
Gio d'Amelio <giodamelio@gmail.com>
2013-12-15 18:45:02 -0800

    Split up sensor black magic into its own file

---
+++ src/main.cpp
@@ -0,0 +1,97 @@
+#include <Arduino.h> // Arduino Stuffs
+#include <Servo.h> // Servo lib for controlling motor controllers
+#include <PID_v1.h> // The Arduino PID lib
+
+#include "main.h"
+#include "sensors.h"
+
+Servo motor_one;
+
+// OUTPUT OPTIONS
+/*****************************************************************/
+// Set your serial port baud rate used to send out data here!
+#define OUTPUT_BAUD_RATE 115200
+
+// Sensor data output interval in milliseconds
+// This may not work, if faster than 20ms (=50Hz)
+// Code is tuned for 20ms, so better leave it like that
+#define OUTPUT_DATA_INTERVAL 20    // in milliseconds
+
+// Quick macros to make some conversions
+#define TO_RAD(x) (x * 0.01745329252)    // *pi/180
+#define TO_DEG(x) (x * 57.2957795131)    // *180/pi
+/*****************************************************************/
+
+double Setpoint, Input, Output;
+// Make the pic                       P, I, D
+PID myPID(&Input, &Output, &Setpoint, 2, 5, 1, DIRECT);
+
+void setup() {
+    // Init serial output
+    Serial.begin(OUTPUT_BAUD_RATE);
+    
+    // Init sensors
+    delay(50);    // Give sensors enough time to start
+    I2C_Init();
+    Accel_Init();
+    Magn_Init();
+    Gyro_Init();
+    Pressure_Init();
+    
+    // Read sensors, init DCM algorithm
+    delay(20);    // Give sensors enough time to collect data
+    reset_sensor_fusion();
+
+    // Set the PID to seek pitch 0
+    Setpoint = 0;
+
+    // Turn the pid on
+    myPID.SetMode(AUTOMATIC);
+    // Set the PIds limits
+    myPID.SetOutputLimits(40, 120);
+ 
+    motor_one.attach(10);
+    
+    motor_one.write(20);
+    delay(5000);
+}
+
+// Main loop
+void loop() {    
+    // Time to read the sensors again?
+    if ((millis() - timestamp) >= OUTPUT_DATA_INTERVAL) {
+        timestamp_old = timestamp;
+        timestamp = millis();
+        if (timestamp > timestamp_old)
+            G_Dt = (float) (timestamp - timestamp_old) / 1000.0f; // Real time of loop run. We use this on the DCM algorithm (gyro integration time)
+        else
+            G_Dt = 0;
+
+        ReadSensors();
+        
+        // Run DCM algorithm
+        Compass_Heading(); // Calculate magnetic heading
+
+        Matrix_update();
+        Normalize();
+        Drift_correction();
+        Euler_angles();
+
+        // Serial.print("Acc: "); Serial.print(gyro[0], DEC);        Serial.print(";     ");
+        // Serial.print("yaw: "); Serial.print(TO_DEG(yaw));        Serial.print(";     ");
+        Serial.print("pitch: "); Serial.print(TO_DEG(pitch)); Serial.print(" ");
+        // Serial.print("roll: "); Serial.print(TO_DEG(roll));     Serial.print(";     ");
+        // Serial.print("temp: "); Serial.print(temperature);        Serial.print(";     ");
+        // Serial.print("pressure: "); Serial.print(pressure);             Serial.print(";     ");
+        // Serial.print("altitude: "); Serial.print(altitude);             
+        
+        
+        Input = TO_DEG(pitch);
+        myPID.Compute();
+        Serial.print("speed: "); Serial.print(Output/2); Serial.print(";    "); Serial.println();
+        motor_one.write(Output/2);
+        
+        delay(20);
+    }
+}
+
--- src/sensors.cpp
+++ src/sensors.cpp
@@ -1,20 +1,10 @@
+#include <Arduino.h>
 #include <Wire.h>
-#include <Servo.h> 
-#include <I2C.h>
-#include <L3G4200D.h>
-#include <PID_v1.h>
+#include <I2C.h> // I2C for the sensor libs
+#include <L3G4200D.h> // The gyro lib
+#include <Adafruit_BMP085.h> // The barometric pressure lib
 
-Servo motor_one;
-
-// OUTPUT OPTIONS
-/*****************************************************************/
-// Set your serial port baud rate used to send out data here!
-#define OUTPUT_BAUD_RATE 115200
-
-// Sensor data output interval in milliseconds
-// This may not work, if faster than 20ms (=50Hz)
-// Code is tuned for 20ms, so better leave it like that
-#define OUTPUT_DATA_INTERVAL 20    // in milliseconds
+#include "sensors.h"
 
 // SENSOR CALIBRATION
 /*****************************************************************/
@@ -71,8 +61,6 @@ Servo motor_one;
 #define GYRO_AVERAGE_OFFSET_Y ((float) 100.41)
 #define GYRO_AVERAGE_OFFSET_Z ((float) -16.38)
 */
-
-#include <Wire.h>
 
 // Sensor calibration scale and offset values
 #define ACCEL_X_OFFSET ((ACCEL_X_MIN + ACCEL_X_MAX) / 2.0f)
@@ -141,181 +129,17 @@ float DCM_Matrix[3][3] = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
 float Update_Matrix[3][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
 float Temporary_Matrix[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
-// Euler angles
-float yaw, pitch, roll;
-
-// DCM timing in the main loop
-long timestamp;
-long timestamp_old;
-float G_Dt; // Integration time for DCM algorithm
-
 // More output-state variables
 int num_accel_errors = 0;
 int num_magn_errors = 0;
 int num_gyro_errors = 0;
 
-void ReadSensors() {
-    Read_Pressure();
-    Read_Gyro(); // Read gyroscope
-    Read_Accel(); // Read accelerometer
-    Read_Magn(); // Read magnetometer
-    ApplySensorMapping();    
-}
-
-// Read every sensor and record a time stamp
-// Init DCM with unfiltered orientation
-// TODO re-init global vars?
-void reset_sensor_fusion()
-{
-    float temp1[3];
-    float temp2[3];
-    float xAxis[] = {1.0f, 0.0f, 0.0f};
-
-    ReadSensors();
-    
-    timestamp = millis();
-    
-    // GET PITCH
-    // Using y-z-plane-component/x-component of gravity vector
-    pitch = -atan2(Accel_Vector[0], sqrt(Accel_Vector[1] * Accel_Vector[1] + Accel_Vector[2] * Accel_Vector[2]));
-                
-    // GET ROLL
-    // Compensate pitch of gravity vector 
-    Vector_Cross_Product(temp1, Accel_Vector, xAxis);
-    Vector_Cross_Product(temp2, xAxis, temp1);
-    // Normally using x-z-plane-component/y-component of compensated gravity vector
-    // roll = atan2(temp2[1], sqrt(temp2[0] * temp2[0] + temp2[2] * temp2[2]));
-    // Since we compensated for pitch, x-z-plane-component equals z-component:
-    roll = atan2(temp2[1], temp2[2]);
-    
-    // GET YAW
-    Compass_Heading();
-    yaw = MAG_Heading;
-    
-    // Init rotation matrix
-    init_rotation_matrix(DCM_Matrix, yaw, pitch, roll);
-}
-
-/* This file is part of the Razor AHRS Firmware */
-
-// DCM algorithm
-
-/**************************************************/
-void Normalize(void)
-{
-    float error=0;
-    float temporary[3][3];
-    float renorm=0;
-    
-    error= -Vector_Dot_Product(&DCM_Matrix[0][0],&DCM_Matrix[1][0])*.5; //eq.19
-
-    Vector_Scale(&temporary[0][0], &DCM_Matrix[1][0], error); //eq.19
-    Vector_Scale(&temporary[1][0], &DCM_Matrix[0][0], error); //eq.19
-    
-    Vector_Add(&temporary[0][0], &temporary[0][0], &DCM_Matrix[0][0]);//eq.19
-    Vector_Add(&temporary[1][0], &temporary[1][0], &DCM_Matrix[1][0]);//eq.19
-    
-    Vector_Cross_Product(&temporary[2][0],&temporary[0][0],&temporary[1][0]); // c= a x b //eq.20
-    
-    renorm= .5 *(3 - Vector_Dot_Product(&temporary[0][0],&temporary[0][0])); //eq.21
-    Vector_Scale(&DCM_Matrix[0][0], &temporary[0][0], renorm);
-    
-    renorm= .5 *(3 - Vector_Dot_Product(&temporary[1][0],&temporary[1][0])); //eq.21
-    Vector_Scale(&DCM_Matrix[1][0], &temporary[1][0], renorm);
-    
-    renorm= .5 *(3 - Vector_Dot_Product(&temporary[2][0],&temporary[2][0])); //eq.21
-    Vector_Scale(&DCM_Matrix[2][0], &temporary[2][0], renorm);
-}
-
-/**************************************************/
-void Drift_correction(void)
-{
-    float mag_heading_x;
-    float mag_heading_y;
-    float errorCourse;
-    //Compensation the Roll, Pitch and Yaw drift. 
-    static float Scaled_Omega_P[3];
-    static float Scaled_Omega_I[3];
-    float Accel_magnitude;
-    float Accel_weight;
-    
-    
-    //*****Roll and Pitch***************
-
-    // Calculate the magnitude of the accelerometer vector
-    Accel_magnitude = sqrt(Accel_Vector[0]*Accel_Vector[0] + Accel_Vector[1]*Accel_Vector[1] + Accel_Vector[2]*Accel_Vector[2]);
-    Accel_magnitude = Accel_magnitude / GRAVITY; // Scale to gravity.
-    // Dynamic weighting of accelerometer info (reliability filter)
-    // Weight for accelerometer info (<0.5G = 0.0, 1G = 1.0 , >1.5G = 0.0)
-    Accel_weight = constrain(1 - 2*abs(1 - Accel_magnitude),0,1);    //    
-
-    Vector_Cross_Product(&errorRollPitch[0],&Accel_Vector[0],&DCM_Matrix[2][0]); //adjust the ground of reference
-    Vector_Scale(&Omega_P[0],&errorRollPitch[0],Kp_ROLLPITCH*Accel_weight);
-    
-    Vector_Scale(&Scaled_Omega_I[0],&errorRollPitch[0],Ki_ROLLPITCH*Accel_weight);
-    Vector_Add(Omega_I,Omega_I,Scaled_Omega_I);         
-    
-    //*****YAW***************
-    // We make the gyro YAW drift correction based on compass magnetic heading
- 
-    mag_heading_x = cos(MAG_Heading);
-    mag_heading_y = sin(MAG_Heading);
-    errorCourse=(DCM_Matrix[0][0]*mag_heading_y) - (DCM_Matrix[1][0]*mag_heading_x);    //Calculating YAW error
-    Vector_Scale(errorYaw,&DCM_Matrix[2][0],errorCourse); //Applys the yaw correction to the XYZ rotation of the aircraft, depeding the position.
-    
-    Vector_Scale(&Scaled_Omega_P[0],&errorYaw[0],Kp_YAW);//.01proportional of YAW.
-    Vector_Add(Omega_P,Omega_P,Scaled_Omega_P);//Adding    Proportional.
-    
-    Vector_Scale(&Scaled_Omega_I[0],&errorYaw[0],Ki_YAW);//.00001Integrator
-    Vector_Add(Omega_I,Omega_I,Scaled_Omega_I);//adding integrator to the Omega_I
-}
-
-void Matrix_update(void)
-{    
-    Vector_Add(&Omega[0], &Gyro_Vector[0], &Omega_I[0]);    //adding proportional term
-    Vector_Add(&Omega_Vector[0], &Omega[0], &Omega_P[0]); //adding Integrator term
-    
-#if DEBUG_NO_DRIFT_CORRECTION
-    Update_Matrix[0][0]=0;
-    Update_Matrix[0][1]=-G_Dt*Gyro_Vector[2];//-z
-    Update_Matrix[0][2]=G_Dt*Gyro_Vector[1];//y
-    Update_Matrix[1][0]=G_Dt*Gyro_Vector[2];//z
-    Update_Matrix[1][1]=0;
-    Update_Matrix[1][2]=-G_Dt*Gyro_Vector[0];
-    Update_Matrix[2][0]=-G_Dt*Gyro_Vector[1];
-    Update_Matrix[2][1]=G_Dt*Gyro_Vector[0];
-    Update_Matrix[2][2]=0;
-#else // Use drift correction
-    Update_Matrix[0][0]=0;
-    Update_Matrix[0][1]=-G_Dt*Omega_Vector[2];//-z
-    Update_Matrix[0][2]=G_Dt*Omega_Vector[1];//y
-    Update_Matrix[1][0]=G_Dt*Omega_Vector[2];//z
-    Update_Matrix[1][1]=0;
-    Update_Matrix[1][2]=-G_Dt*Omega_Vector[0];//-x
-    Update_Matrix[2][0]=-G_Dt*Omega_Vector[1];//-y
-    Update_Matrix[2][1]=G_Dt*Omega_Vector[0];//x
-    Update_Matrix[2][2]=0;
-#endif
-
-    Matrix_Multiply(DCM_Matrix,Update_Matrix,Temporary_Matrix); //a*b=c
-
-    for(int x=0; x<3; x++) //Matrix Addition (update)
-    {
-        for(int y=0; y<3; y++)
-        {
-            DCM_Matrix[x][y]+=Temporary_Matrix[x][y];
-        } 
-    }
-}
-
-void Euler_angles(void)
-{
-    pitch = -asin(DCM_Matrix[2][0]);
-    roll = atan2(DCM_Matrix[2][1],DCM_Matrix[2][2]);
-    yaw = atan2(DCM_Matrix[1][0],DCM_Matrix[0][0]);
-}
-
-/* This file is part of the Razor AHRS Firmware */
+float yaw;
+float pitch;
+float roll;
+long timestamp;
+long timestamp_old;
+float G_Dt;
 
 // Computes the dot product of two vectors
 float Vector_Dot_Product(float vector1[3], float vector2[3])
@@ -374,6 +198,119 @@ void Matrix_Multiply(float a[3][3], float b[3][3],float mat[3][3])
             float test=mat[x][y];
         }
     }
+}
+
+void Matrix_update(void)
+{    
+    Vector_Add(&Omega[0], &Gyro_Vector[0], &Omega_I[0]);    //adding proportional term
+    Vector_Add(&Omega_Vector[0], &Omega[0], &Omega_P[0]); //adding Integrator term
+    
+#if DEBUG_NO_DRIFT_CORRECTION
+    Update_Matrix[0][0]=0;
+    Update_Matrix[0][1]=-G_Dt*Gyro_Vector[2];//-z
+    Update_Matrix[0][2]=G_Dt*Gyro_Vector[1];//y
+    Update_Matrix[1][0]=G_Dt*Gyro_Vector[2];//z
+    Update_Matrix[1][1]=0;
+    Update_Matrix[1][2]=-G_Dt*Gyro_Vector[0];
+    Update_Matrix[2][0]=-G_Dt*Gyro_Vector[1];
+    Update_Matrix[2][1]=G_Dt*Gyro_Vector[0];
+    Update_Matrix[2][2]=0;
+#else // Use drift correction
+    Update_Matrix[0][0]=0;
+    Update_Matrix[0][1]=-G_Dt*Omega_Vector[2];//-z
+    Update_Matrix[0][2]=G_Dt*Omega_Vector[1];//y
+    Update_Matrix[1][0]=G_Dt*Omega_Vector[2];//z
+    Update_Matrix[1][1]=0;
+    Update_Matrix[1][2]=-G_Dt*Omega_Vector[0];//-x
+    Update_Matrix[2][0]=-G_Dt*Omega_Vector[1];//-y
+    Update_Matrix[2][1]=G_Dt*Omega_Vector[0];//x
+    Update_Matrix[2][2]=0;
+#endif
+
+    Matrix_Multiply(DCM_Matrix,Update_Matrix,Temporary_Matrix); //a*b=c
+
+    for(int x=0; x<3; x++) //Matrix Addition (update)
+    {
+        for(int y=0; y<3; y++)
+        {
+            DCM_Matrix[x][y]+=Temporary_Matrix[x][y];
+        } 
+    }
+}
+
+void Euler_angles(void)
+{
+    pitch = -asin(DCM_Matrix[2][0]);
+    roll = atan2(DCM_Matrix[2][1],DCM_Matrix[2][2]);
+    yaw = atan2(DCM_Matrix[1][0],DCM_Matrix[0][0]);
+}
+
+void Normalize(void)
+{
+    float error=0;
+    float temporary[3][3];
+    float renorm=0;
+    
+    error= -Vector_Dot_Product(&DCM_Matrix[0][0],&DCM_Matrix[1][0])*.5; //eq.19
+
+    Vector_Scale(&temporary[0][0], &DCM_Matrix[1][0], error); //eq.19
+    Vector_Scale(&temporary[1][0], &DCM_Matrix[0][0], error); //eq.19
+    
+    Vector_Add(&temporary[0][0], &temporary[0][0], &DCM_Matrix[0][0]);//eq.19
+    Vector_Add(&temporary[1][0], &temporary[1][0], &DCM_Matrix[1][0]);//eq.19
+    
+    Vector_Cross_Product(&temporary[2][0],&temporary[0][0],&temporary[1][0]); // c= a x b //eq.20
+    
+    renorm= .5 *(3 - Vector_Dot_Product(&temporary[0][0],&temporary[0][0])); //eq.21
+    Vector_Scale(&DCM_Matrix[0][0], &temporary[0][0], renorm);
+    
+    renorm= .5 *(3 - Vector_Dot_Product(&temporary[1][0],&temporary[1][0])); //eq.21
+    Vector_Scale(&DCM_Matrix[1][0], &temporary[1][0], renorm);
+    
+    renorm= .5 *(3 - Vector_Dot_Product(&temporary[2][0],&temporary[2][0])); //eq.21
+    Vector_Scale(&DCM_Matrix[2][0], &temporary[2][0], renorm);
+}
+
+void Drift_correction(void)
+{
+    float mag_heading_x;
+    float mag_heading_y;
+    float errorCourse;
+    //Compensation the Roll, Pitch and Yaw drift. 
+    static float Scaled_Omega_P[3];
+    static float Scaled_Omega_I[3];
+    float Accel_magnitude;
+    float Accel_weight;
+    
+    
+    //*****Roll and Pitch***************
+
+    // Calculate the magnitude of the accelerometer vector
+    Accel_magnitude = sqrt(Accel_Vector[0]*Accel_Vector[0] + Accel_Vector[1]*Accel_Vector[1] + Accel_Vector[2]*Accel_Vector[2]);
+    Accel_magnitude = Accel_magnitude / GRAVITY; // Scale to gravity.
+    // Dynamic weighting of accelerometer info (reliability filter)
+    // Weight for accelerometer info (<0.5G = 0.0, 1G = 1.0 , >1.5G = 0.0)
+    Accel_weight = constrain(1 - 2*abs(1 - Accel_magnitude),0,1);    //    
+
+    Vector_Cross_Product(&errorRollPitch[0],&Accel_Vector[0],&DCM_Matrix[2][0]); //adjust the ground of reference
+    Vector_Scale(&Omega_P[0],&errorRollPitch[0],Kp_ROLLPITCH*Accel_weight);
+    
+    Vector_Scale(&Scaled_Omega_I[0],&errorRollPitch[0],Ki_ROLLPITCH*Accel_weight);
+    Vector_Add(Omega_I,Omega_I,Scaled_Omega_I);         
+    
+    //*****YAW***************
+    // We make the gyro YAW drift correction based on compass magnetic heading
+ 
+    mag_heading_x = cos(MAG_Heading);
+    mag_heading_y = sin(MAG_Heading);
+    errorCourse=(DCM_Matrix[0][0]*mag_heading_y) - (DCM_Matrix[1][0]*mag_heading_x);    //Calculating YAW error
+    Vector_Scale(errorYaw,&DCM_Matrix[2][0],errorCourse); //Applys the yaw correction to the XYZ rotation of the aircraft, depeding the position.
+    
+    Vector_Scale(&Scaled_Omega_P[0],&errorYaw[0],Kp_YAW);//.01proportional of YAW.
+    Vector_Add(Omega_P,Omega_P,Scaled_Omega_P);//Adding    Proportional.
+    
+    Vector_Scale(&Scaled_Omega_I[0],&errorYaw[0],Ki_YAW);//.00001Integrator
+    Vector_Add(Omega_I,Omega_I,Scaled_Omega_I);//adding integrator to the Omega_I
 }
 
 // Init rotation matrix using euler angles
@@ -517,7 +454,7 @@ void Read_Magn()
     }
 }
 
-#include <L3G4200D.h>
+
 
 L3G4200D gyroscope;
 
@@ -536,8 +473,6 @@ void Read_Gyro()
     gyro[1] = gyroscope.g.y;
     gyro[2] = gyroscope.g.z;
 }
-
-#include <Adafruit_BMP085.h>
 
 Adafruit_BMP085 press;
 
@@ -619,78 +554,44 @@ void Compass_Heading()
     MAG_Heading = atan2(-mag_y, mag_x);
 }
 
-double Setpoint, Input, Output;
-//                                    P, I, D
-PID myPID(&Input, &Output, &Setpoint, 2, 5, 1, DIRECT);
+void ReadSensors() {
+    Read_Pressure();
+    Read_Gyro(); // Read gyroscope
+    Read_Accel(); // Read accelerometer
+    Read_Magn(); // Read magnetometer
+    ApplySensorMapping();    
+}
 
-void setup()
+// Read every sensor and record a time stamp
+// Init DCM with unfiltered orientation
+// TODO re-init global vars?
+void reset_sensor_fusion()
 {
-    // Init serial output
-    Serial.begin(OUTPUT_BAUD_RATE);
-    
-    // Init sensors
-    delay(50);    // Give sensors enough time to start
-    I2C_Init();
-    Accel_Init();
-    Magn_Init();
-    Gyro_Init();
-    Pressure_Init();
-    
-    // Read sensors, init DCM algorithm
-    delay(20);    // Give sensors enough time to collect data
-    reset_sensor_fusion();
+    float temp1[3];
+    float temp2[3];
+    float xAxis[] = {1.0f, 0.0f, 0.0f};
 
-    // Set the PID to seek pitch 0
-    Setpoint = 0;
-
-    // Turn the pid on
-    myPID.SetMode(AUTOMATIC);
-    // Set the PIds limits
-    myPID.SetOutputLimits(40, 120);
- 
-    motor_one.attach(10);
+    ReadSensors();
     
-    motor_one.write(20);
-    delay(5000);
+    timestamp = millis();
+    
+    // GET PITCH
+    // Using y-z-plane-component/x-component of gravity vector
+    pitch = -atan2(Accel_Vector[0], sqrt(Accel_Vector[1] * Accel_Vector[1] + Accel_Vector[2] * Accel_Vector[2]));
+                
+    // GET ROLL
+    // Compensate pitch of gravity vector 
+    Vector_Cross_Product(temp1, Accel_Vector, xAxis);
+    Vector_Cross_Product(temp2, xAxis, temp1);
+    // Normally using x-z-plane-component/y-component of compensated gravity vector
+    // roll = atan2(temp2[1], sqrt(temp2[0] * temp2[0] + temp2[2] * temp2[2]));
+    // Since we compensated for pitch, x-z-plane-component equals z-component:
+    roll = atan2(temp2[1], temp2[2]);
+    
+    // GET YAW
+    Compass_Heading();
+    yaw = MAG_Heading;
+    
+    // Init rotation matrix
+    init_rotation_matrix(DCM_Matrix, yaw, pitch, roll);
 }
-
-
-// Main loop
-void loop() {    
-    // Time to read the sensors again?
-    if ((millis() - timestamp) >= OUTPUT_DATA_INTERVAL) {
-        timestamp_old = timestamp;
-        timestamp = millis();
-        if (timestamp > timestamp_old)
-            G_Dt = (float) (timestamp - timestamp_old) / 1000.0f; // Real time of loop run. We use this on the DCM algorithm (gyro integration time)
-        else
-            G_Dt = 0;
-
-        ReadSensors();
-        
-        // Run DCM algorithm
-        Compass_Heading(); // Calculate magnetic heading
-
-        Matrix_update();
-        Normalize();
-        Drift_correction();
-        Euler_angles();
-
-        // Serial.print("Acc: "); Serial.print(gyro[0], DEC);        Serial.print(";     ");
-        // Serial.print("yaw: "); Serial.print(TO_DEG(yaw));        Serial.print(";     ");
-        Serial.print("pitch: "); Serial.print(TO_DEG(pitch)); Serial.print(" ");
-        // Serial.print("roll: "); Serial.print(TO_DEG(roll));     Serial.print(";     ");
-        // Serial.print("temp: "); Serial.print(temperature);        Serial.print(";     ");
-        // Serial.print("pressure: "); Serial.print(pressure);             Serial.print(";     ");
-        // Serial.print("altitude: "); Serial.print(altitude);             
-        
-        
-        Input = TO_DEG(pitch);
-        myPID.Compute();
-        Serial.print("speed: "); Serial.print(Output/2); Serial.print(";    "); Serial.println();
-        motor_one.write(Output/2);
-        
-        delay(20);
-    }
-}
-
